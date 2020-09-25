deb55b9ddb480bcf2c9166f9fb27fb463b7d2b76
Denis Brandolini <denis.brandolini@prometeia.com>
2020-09-25 15:28:47 +0200

    fixup

--- gegirun.sh
+++ gegirun.sh
@@ -3,4 +3,4 @@ conda install pipenv -n geminhack -c conda-forge -y
 CONDABIN=$(conda info --envs | grep geminhack | sed 's/[[:blank:]]\+/ /' | cut -d ' ' -f 2)/bin
 $CONDABIN/pipenv sync --python $CONDABIN/python3.8
 $CONDABIN/pipenv run python -m geminhack.ge2gi $GEMINHAACKDB
-$CONDABIN env remove -n geminhack
+conda env remove -n geminhack
