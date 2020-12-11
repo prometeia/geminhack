4da8d4c6c2f126b75d53f7ed9b261d66203f223d
Denis Brandolini <denis.brandolini@prometeia.com>
2020-12-11 12:59:10 +0100

    forgot the csv

--- gegirun.sh
+++ gegirun.sh
@@ -2,7 +2,7 @@ conda create -n geminhack python=3.8 -c conda-forge -y
 conda install pipenv -n geminhack -c conda-forge -y
 CONDABIN=$(conda info --envs | grep geminhack | sed 's/[[:blank:]]\+/ /' | cut -d ' ' -f 2)/bin
 $CONDABIN/pipenv sync --python $CONDABIN/python3.8
-$CONDABIN/pipenv run python -m geminhack.ge2gi $GEMINHAACKDB MAIN
+$CONDABIN/pipenv run python -m geminhack.ge2gi $GEMINHAACKDB MAIN --quotefile "sprintboard.csv"
 #$CONDABIN/pipenv run python -m geminhack.ge2gi $GEMINHAACKDB UI
 $CONDABIN/pipenv run python -m geminhack.ge2gi $GEMINHAACKDB SBB
 conda env remove -n geminhack
